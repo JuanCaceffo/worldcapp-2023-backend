eb5ab55c6db554b16a50eb351fe1e7d7cd2e92da
madescoces <madescoces@gmail.com>
2023-10-14 16:04:26 -0300

    First commit

--- src/main/kotlin/ar/edu/unsam/algo3/domain/FiguritaFilter.kt
+++ src/main/kotlin/ar/edu/unsam/algo3/domain/FiguritaFilter.kt
@@ -1,4 +1,9 @@
 package ar.edu.unsam.algo3.domain
 
-class FiguritaFilter {
-}
+data class FiguritaFilter (
+  var palabraClave: String = "",
+  var onFire: Boolean? = false,
+  var esPromesa: Boolean? = false,
+  var cotizacionInicial: Double? = 0.0,
+  var cotizacionFinal: Double? = 0.0
+)
--- src/main/kotlin/ar/edu/unsam/algo3/service/FiguritaService.kt
+++ src/main/kotlin/ar/edu/unsam/algo3/service/FiguritaService.kt
@@ -1,6 +1,7 @@
 package ar.edu.unsam.algo3.service
 
 import ar.edu.unsam.algo3.domain.Figurita
+import ar.edu.unsam.algo3.domain.FiguritaFilter
 import ar.edu.unsam.algo3.domain.Usuario
 import ar.edu.unsam.algo3.repository.Repositorio
 import org.springframework.stereotype.Service
@@ -10,5 +11,21 @@ class FiguritaService (
   val figuritaRepository: Repositorio<Figurita>,
   val usuarioRepository: Repositorio<Usuario>
 ){
-  fun search(textoBusqueda: String) = figuritaRepository.search(textoBusqueda)
+  fun search(figuritaFilter: FiguritaFilter):List<Figurita> {
+    var listaFiltrada = figuritaRepository.search(figuritaFilter.palabraClave)
+
+    if(figuritaFilter.onFire!!) {
+      listaFiltrada.filter { it.estaOnfire() }
+    }
+
+    if(figuritaFilter.esPromesa!!) {
+      listaFiltrada.filter { it.jugador.promesaDelFutbol() }
+    }
+
+    if(figuritaFilter.cotizacionFinal!! > 0.0) {
+      listaFiltrada.filter { it.valoracion() in figuritaFilter.cotizacionInicial!!..figuritaFilter.cotizacionFinal!!}
+    }
+
+    return listaFiltrada
+  }
 }
