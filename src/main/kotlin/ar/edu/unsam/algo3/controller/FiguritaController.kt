7f6ad3d7ca6548099f17eacb8adc5c8e2299d1ef
madescoces <madescoces@gmail.com>
2023-10-15 07:35:59 -0300

    Correción de bugs 500 que lanzaban un error al enviar peticiones personalizadas por parametros null

--- src/main/kotlin/ar/edu/unsam/algo3/controller/FiguritaController.kt
+++ src/main/kotlin/ar/edu/unsam/algo3/controller/FiguritaController.kt
@@ -14,21 +14,21 @@ class FiguritaController (val figuritaService: FiguritaService){
   @GetMapping("/figuritas/search")
   @Operation(summary="Permite buscar figuritas en base al criterio de búsqueda pasado")
   fun search(
-    @RequestParam(required = false) palabraClaveFigurita: String?,
-    @RequestParam(required = false) onFireFigurita: Boolean?,
-    @RequestParam(required = false) esPromesaFigurita: Boolean?,
-    @RequestParam(required = false) cotizacionInicialFigurita: Double?,
-    @RequestParam(required = false) cotizacionFinalFigurita: Double?
+    @RequestParam(required = false) palabraClaveFigurita: String? = null,
+    @RequestParam(required = false) onFireFigurita: Boolean = false,
+    @RequestParam(required = false) esPromesaFigurita: Boolean = false,
+    @RequestParam(required = false) cotizacionInicialFigurita: Double? = null,
+    @RequestParam(required = false) cotizacionFinalFigurita: Double? = null
   ):List<Figurita> {
-    return if (palabraClaveFigurita == null && onFireFigurita == null && esPromesaFigurita == null && cotizacionInicialFigurita == null && cotizacionFinalFigurita == null) {
+    return if (palabraClaveFigurita == "" && !onFireFigurita && !esPromesaFigurita && cotizacionInicialFigurita == 0.0 && cotizacionFinalFigurita == 0.0) {
       // Devolver toda la lista de figuritas sin filtro
       figuritaService.getAll()
     } else {
       figuritaService.search(FiguritaFilter().apply{
-        palabraClave=palabraClaveFigurita
-        onFire=onFireFigurita!!
-        esPromesa=esPromesaFigurita!!
-        rangoCotizacion=(cotizacionInicialFigurita!!..cotizacionFinalFigurita!!)
+        palabraClave = palabraClaveFigurita ?: ""
+        onFire = onFireFigurita
+        esPromesa = esPromesaFigurita
+        rangoCotizacion = (cotizacionInicialFigurita ?: 0.0)..(cotizacionFinalFigurita ?: 0.0)
       })
     }
   }
