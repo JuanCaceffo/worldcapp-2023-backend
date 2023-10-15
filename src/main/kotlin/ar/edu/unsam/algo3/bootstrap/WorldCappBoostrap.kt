624773cba46d2f2272cc0a9617a4c9c5c6ffbf47
madescoces <madescoces@gmail.com>
2023-10-15 06:33:27 -0300

    Se terminan de agregar las figuritas al boostrap

--- src/main/kotlin/ar/edu/unsam/algo3/bootstrap/WorldCappBoostrap.kt
+++ src/main/kotlin/ar/edu/unsam/algo3/bootstrap/WorldCappBoostrap.kt
@@ -18,6 +18,8 @@ class WorldCappBoostrap (
 ): InitializingBean {
 
   private lateinit var argentina: Seleccion
+  private lateinit var espania: Seleccion
+  private lateinit var brasil: Seleccion
   private lateinit var polonia: Seleccion
   private lateinit var belgica: Seleccion
   private lateinit var holanda: Seleccion
@@ -30,6 +32,12 @@ class WorldCappBoostrap (
   private lateinit var ofc: Confederacion
   private lateinit var uefa: Confederacion
 
+  private lateinit var martinez: Jugador
+  private lateinit var otamendi: Jugador
+  private lateinit var argento: Jugador
+  private lateinit var messi: Jugador
+  private lateinit var ramos: Jugador
+  private lateinit var neymar: Jugador
   private lateinit var lewandowski: Jugador
   private lateinit var hazard: Jugador
   private lateinit var debruyne: Jugador
@@ -38,7 +46,12 @@ class WorldCappBoostrap (
 
   fun crearFiguritas() {
     figuritasRepository.apply {
-      create(Figurita(numero=1, onFire=true, cantidadImpresa=impresionBaja, jugador=jugadorRepository.getById(0)!!))
+      create(Figurita(numero=1, onFire=false, cantidadImpresa=impresionBaja, jugador=martinez))
+      create(Figurita(numero=2, onFire=false, cantidadImpresa= impresionAlta, jugador=otamendi))
+      create(Figurita(numero=3, onFire=true, cantidadImpresa= impresionMedia, jugador=argento))
+      create(Figurita(numero=4, onFire=true, cantidadImpresa= impresionBaja, jugador=messi))
+      create(Figurita(numero=5, onFire=false, cantidadImpresa= impresionAlta, jugador=ramos))
+      create(Figurita(numero=6, onFire=false, cantidadImpresa= impresionBaja, jugador=neymar))
       create(Figurita(numero=8, onFire=true, cantidadImpresa= impresionAlta, jugador=lewandowski))
       create(Figurita(numero=15, onFire=true, cantidadImpresa= impresionAlta, jugador=hazard))
       create(Figurita(numero=6, onFire=false, cantidadImpresa= impresionAlta, jugador=debruyne))
@@ -48,6 +61,12 @@ class WorldCappBoostrap (
   }
 
   private fun crearJugador() {
+    martinez = Jugador(nombre = "Gonzalo", apellido = "Martinez", fechaNacimiento = LocalDate.of(1993, 6,13), nroCamiseta = 10, seleccionPerteneciente = argentina, posicion=Mediocampista, anioDeDebut=2008, altura=1.72, peso=70.0, esLider=true, cotizacion=9000000.0 )
+    otamendi = Jugador(nombre = "Nicolas", apellido = "Otamendi", fechaNacimiento = LocalDate.of(1998, 12,2), nroCamiseta = 3, seleccionPerteneciente = argentina, posicion=Defensor, anioDeDebut=2003, altura=1.83, peso=75.0, esLider=false, cotizacion=1200000.0 )
+    argento = Jugador(nombre = "Pepe", apellido = "Argento", fechaNacimiento = LocalDate.of(1993, 2,12), nroCamiseta = 5, seleccionPerteneciente = argentina, posicion=Arquero, anioDeDebut=2012, altura=1.69, peso=69.0, esLider=true, cotizacion=1700000.0 )
+    messi = Jugador(nombre = "Lionel", apellido = "Messi", fechaNacimiento = LocalDate.of(1987, 6,24), nroCamiseta = 10, seleccionPerteneciente = argentina, posicion=Delantero, anioDeDebut=2004, altura=1.7, peso=69.0, esLider=true, cotizacion=19003000.0 )
+    ramos = Jugador(nombre = "Sergio", apellido = "Ramos", fechaNacimiento = LocalDate.of(1986, 3,30), nroCamiseta = 4, seleccionPerteneciente = espania, posicion=Defensor, anioDeDebut=2002, altura=1.84, peso=75.0, esLider=false, cotizacion=1035000.0 )
+    neymar = Jugador(nombre = "Jr", apellido = "Neymar", fechaNacimiento = LocalDate.of(1992, 2,5), nroCamiseta = 11, seleccionPerteneciente = brasil, posicion=Delantero, anioDeDebut=2010, altura=1.75, peso=71.0, esLider=false, cotizacion=39000000.0 )
     lewandowski = Jugador(nombre = "Robert", apellido = "Lewandowski", fechaNacimiento = LocalDate.of(1988, 8,21), nroCamiseta = 9, seleccionPerteneciente = polonia, posicion=Delantero, anioDeDebut=2006, altura=1.84, peso=80.0, esLider=true, cotizacion=1600000.0 )
     hazard = Jugador(nombre = "Eden", apellido = "Hazard", fechaNacimiento = LocalDate.of(1991, 1,7), nroCamiseta = 7, seleccionPerteneciente = belgica, posicion=Delantero, anioDeDebut=2006, altura=1.73, peso=73.0, esLider=true, cotizacion=1100000.0 )
     debruyne = Jugador(nombre = "Kevin", apellido = "De Bruyne", fechaNacimiento = LocalDate.of(1991, 6,28), nroCamiseta = 17, seleccionPerteneciente = belgica, posicion=Mediocampista, anioDeDebut=2008, altura=1.81, peso=76.0, esLider=false, cotizacion=1200000.0 )
@@ -55,7 +74,12 @@ class WorldCappBoostrap (
     mbappe = Jugador(nombre = "Kylian", apellido = "Mbappé", fechaNacimiento = LocalDate.of(1998, 12,20), nroCamiseta = 7, seleccionPerteneciente = francia, posicion=Delantero, anioDeDebut=2016, altura=1.78, peso=74.0, esLider=false, cotizacion=1700000.0 )
 
     jugadorRepository.apply {
-      create(Jugador(nombre = "Gonzalo", apellido = "Martinez", fechaNacimiento = LocalDate.of(1993, 6,13), nroCamiseta = 10, seleccionPerteneciente = argentina, posicion=Mediocampista, anioDeDebut=2008, altura=1.72, peso=70.0, esLider=true, cotizacion=9000000.0 ) )
+      create(martinez)
+      create(otamendi)
+      create(argento)
+      create(messi)
+      create(ramos)
+      create(neymar)
       create(lewandowski)
       create(hazard)
       create(debruyne)
@@ -66,10 +90,12 @@ class WorldCappBoostrap (
 
   private fun crearSelecciones() {
     argentina = Seleccion(pais="Argentina", conmebol, copasConfederacion=22, copasDelMundo=3 )
-    polonia = Seleccion(pais="Polonia", uefa, copasConfederacion = 7, copasDelMundo = 3)
-    belgica = Seleccion(pais="Belgica", uefa, copasConfederacion = 3, copasDelMundo = 2)
-    holanda = Seleccion(pais = "Holanda", uefa, copasConfederacion = 4, copasDelMundo = 2)
-    francia = Seleccion(pais = "Francia", uefa, copasConfederacion = 2, copasDelMundo = 1)
+    brasil = Seleccion(pais="Brasil", conmebol, copasConfederacion=22, copasDelMundo=5 )
+    espania = Seleccion(pais="España", uefa, copasConfederacion=8, copasDelMundo=4)
+    polonia = Seleccion(pais="Polonia", uefa, copasConfederacion=7, copasDelMundo=3)
+    belgica = Seleccion(pais="Belgica", uefa, copasConfederacion=3, copasDelMundo=2)
+    holanda = Seleccion(pais="Holanda", uefa, copasConfederacion=4, copasDelMundo=2)
+    francia = Seleccion(pais="Francia", uefa, copasConfederacion=2, copasDelMundo=1)
 
     seleccionesRepository.apply {
       create(argentina)
