dc4d9393a7d3e53477fac7d34f54770edc8e445c
JuanCaceffo <caceffojuan@gmail.com>
2023-10-18 12:07:43 -0300

    Creacion del endpoint para solicitar una figurita

--- src/main/kotlin/ar/edu/unsam/algo3/controller/userController.kt
+++ src/main/kotlin/ar/edu/unsam/algo3/controller/userController.kt
@@ -1,12 +1,14 @@
 package ar.edu.unsam.algo3.controller
 
 import ar.edu.unsam.algo3.domain.Usuario
+import ar.edu.unsam.algo3.dto.RequestFiguDTO
 import ar.edu.unsam.algo3.dto.UsuarioLogeadoDTO
 import ar.edu.unsam.algo3.dto.UsuarioLoginDTO
 import ar.edu.unsam.algo3.service.UsuarioService
 import io.swagger.v3.oas.annotations.Operation
 import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -21,5 +23,11 @@ class UserController(val userService: UsuarioService) {
 
     @GetMapping("/user/{id}")
     @Operation(summary = "Permite buscar un usuario por ID")
+    //TODO: Serializar datos necesarios del usuario para el fornt
     fun searchUser(@PathVariable id: Int): Usuario = userService.searchByID(id)
+
+    @PatchMapping("user/request-figurita")
+    @Operation(summary = "Permite realizar una solicitud de una figurita a un usuario")
+    fun figuritaRequest(@RequestBody requestData: RequestFiguDTO) = userService.figuritaRequest(requestData)
+
 }
--- src/main/kotlin/ar/edu/unsam/algo3/service/UsuarioService.kt
+++ src/main/kotlin/ar/edu/unsam/algo3/service/UsuarioService.kt
@@ -1,22 +1,37 @@
 package ar.edu.unsam.algo3.service
 
 import ar.edu.unsam.algo3.domain.Usuario
+import ar.edu.unsam.algo3.dto.RequestFiguDTO
 import ar.edu.unsam.algo3.dto.UsuarioLogeadoDTO
 import ar.edu.unsam.algo3.dto.UsuarioLoginDTO
 import ar.edu.unsam.algo3.dto.loginResponseDTO
 import ar.edu.unsam.algo3.error.NotFoundException
 import ar.edu.unsam.algo3.repository.UsuariosRepository
 import org.springframework.stereotype.Service
-import kotlin.reflect.typeOf
+
+
+val ERROR_MSG_INVALID_CREDENTIALS = "Las credenciales suministradas son incorrectas"
+val ERROR_MSG_INVALID_REQUESTED_FIGU = "El usuario no posee la figurita solicitada"
 
 @Service
 class UsuarioService(val usuarioRepo: UsuariosRepository) {
     fun login(dataUser: UsuarioLoginDTO): UsuarioLogeadoDTO {
         //TODO: hacer el filtrado en repository para no guardar todos los usarios en memoria del sv
-        val user = usuarioRepo.getAll().find { user -> user.nombreUsuario == dataUser.userName && user.contrasenia == dataUser.password}
-            ?: throw NotFoundException("Las credenciales suministradas son incorrectas")
+        val user = usuarioRepo.getAll()
+            .find { user -> user.nombreUsuario == dataUser.userName && user.contrasenia == dataUser.password }
+            ?: throw NotFoundException(ERROR_MSG_INVALID_CREDENTIALS)
         return user.loginResponseDTO()
     }
+
     fun searchByID(id: Int): Usuario = usuarioRepo.getById(id)
+
+    fun figuritaRequest(requestData: RequestFiguDTO) {
+        val logedUser = searchByID(requestData.userLogedID)
+        val userRequested = searchByID(requestData.requestedUserID)
+        val figuRequested = userRequested.listaFiguritasARegalar().find { it.id == requestData.requestedFiguID }
+            ?: throw NotFoundException(ERROR_MSG_INVALID_REQUESTED_FIGU)
+
+        userRequested.pedirFigurita(figuRequested,logedUser)
+    }
 
 }
