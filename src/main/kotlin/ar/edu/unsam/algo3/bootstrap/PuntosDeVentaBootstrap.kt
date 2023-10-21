d6b467508e5b38cbf0ce1e14435499785dfdcf5b
Facundo Barneche <fh.barneche@gmail.com>
2023-10-20 21:39:37 -0300

    fix:Cambio valores ubicacion puntos de ventas iniciales

--- src/main/kotlin/ar/edu/unsam/algo3/bootstrap/PuntosDeVentaBootstrap.kt
+++ src/main/kotlin/ar/edu/unsam/algo3/bootstrap/PuntosDeVentaBootstrap.kt
@@ -14,15 +14,15 @@ class PuntosDeVentaBootstrap(
     val usuariosRepositorio: UsuariosRepository
 
 ): InitializingBean {
-    val kiosko1 = Kioscos("La Scaloneta", Direccion("bs as","3 de Febrero", "rodriguez peña", 211, Point(-34.5, -58.5)),2, false)
-    val kiosko2 = Kioscos("Señor Kioskero", Direccion("bs as","3 de Febrero", "Urquiza", 31, Point(-34.6, -58.5)),20, true)
-    val kiosko3 = Kioscos("Un Nombre", Direccion("bs as","Nuñez", "Figueroa Alcorta", 1000, Point(-34.7, -58.5)),1, true)
-    val mercado1 = Supermercados("Chinito", Direccion("bs as","3 de Febrero", "Brasil", 211, Point(-34.0, -58.5)),0, SinDescuento())
-    val mercado2 = Supermercados("Polque", Direccion("bs as","San Martin", "Bolivia", 211, Point(-34.5, -58.0)),10, SinDescuento())
-    val mercado3 = Supermercados("No hay polque", Direccion("bs as","Liniers", "J. B. Justo", 211, Point(-34.5, -58.9)),30, SinDescuento())
-    val libreria1 = Librerias("El Principito", Direccion("bs as","3 de Febrero", "Giorello", 211, Point(-34.5, -58.6)),30)
-    val libreria2 = Librerias("Roberto Carlos", Direccion("bs as","Palermo", "Av. Libertador", 3000, Point(-34.9, -55.5)),31)
-    val libreria3 = Librerias("Lapicito", Direccion("bs as","Belgrano", "Av. Cabildo", 2121, Point(-35.5, -59.5)),230)
+    val kiosko1 = Kioscos("La Scaloneta", Direccion("bs as","3 de Febrero", "rodriguez peña", 211, Point(-34.43123068891678, -58.13359068891678)),2, false)
+    val kiosko2 = Kioscos("Señor Kioskero", Direccion("bs as","3 de Febrero", "Urquiza", 31, Point(-34.1111906555678, -58.11111891678)),20, true)
+    val kiosko3 = Kioscos("Un Nombre", Direccion("bs as","Nuñez", "Figueroa Alcorta", 1000, Point(-34.433590688678, -58.2891678)),1, true)
+    val mercado1 = Supermercados("Chinito", Direccion("bs as","3 de Febrero", "Brasil", 211, Point(-34.433891678, -58.43359068891222)),0, SinDescuento())
+    val mercado2 = Supermercados("Polque", Direccion("bs as","San Martin", "Bolivia", 211, Point(-34.43359458891678, -58.43359068891)),10, SinDescuento())
+    val mercado3 = Supermercados("No hay polque", Direccion("bs as","Liniers", "J. B. Justo", 211, Point(-34.3359068891678, -58.43359064561678)),30, SinDescuento())
+    val libreria1 = Librerias("El Principito", Direccion("bs as","3 de Febrero", "Giorello", 211, Point(-34.3459068891678, -58.43385645691678)),30)
+    val libreria2 = Librerias("Roberto Carlos", Direccion("bs as","Palermo", "Av. Libertador", 3000, Point(-34.1119068891678, -55.433586891678)),31)
+    val libreria3 = Librerias("Lapicito", Direccion("bs as","Belgrano", "Av. Cabildo", 2121, Point(-35.9993590688916, -59.5906889167888)),230)
 
     fun createPuntosDeVentas() {
         puntosDeVentaRepository.apply {
