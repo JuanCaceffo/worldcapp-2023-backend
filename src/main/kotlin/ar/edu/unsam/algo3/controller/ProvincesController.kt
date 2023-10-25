fe0792b93820952a18c0a52c6fd7c1d179a0bb3f
Facundo Barneche <fh.barneche@gmail.com>
2023-10-25 07:57:06 -0300

    feature:Se implementa controlador para provincias

---
+++ src/main/kotlin/ar/edu/unsam/algo3/controller/ProvincesController.kt
@@ -0,0 +1,17 @@
+package ar.edu.unsam.algo3.controller
+
+import ar.edu.unsam.algo3.service.Province
+import ar.edu.unsam.algo3.service.ProvinceService
+import io.swagger.v3.oas.annotations.Operation
+import org.springframework.web.bind.annotation.CrossOrigin
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@CrossOrigin("*")
+class ProvincesController(val provinceService: ProvinceService) {
+    @GetMapping("/provinces")
+    @Operation(summary = "Obtiene todos las provincias con sus respectivas localidades")
+    fun getAll(): List<Province> = this.provinceService.getAll()
+}
