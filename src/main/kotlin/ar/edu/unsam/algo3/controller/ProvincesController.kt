76860e0e1c9791fc7402fd3ab1d56631deee0fa1
Facundo Barneche <fh.barneche@gmail.com>
2023-10-25 22:12:37 -0300

    fix:Se agregan localidades necesarias para buenos aires

--- src/main/kotlin/ar/edu/unsam/algo3/controller/ProvincesController.kt
+++ src/main/kotlin/ar/edu/unsam/algo3/controller/ProvincesController.kt
@@ -5,7 +5,6 @@ import ar.edu.unsam.algo3.service.ProvinceService
 import io.swagger.v3.oas.annotations.Operation
 import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
