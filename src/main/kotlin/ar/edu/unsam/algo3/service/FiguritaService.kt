1369b5b3d579642b3a5d352885f57d3ae9f47e44
JuanCaceffo <caceffojuan@gmail.com>
2023-10-30 16:21:28 -0300

    separacaion de la logica de filtrado en figurita para poder reutilizarla

--- src/main/kotlin/ar/edu/unsam/algo3/service/FiguritaService.kt
+++ src/main/kotlin/ar/edu/unsam/algo3/service/FiguritaService.kt
@@ -2,6 +2,7 @@ package ar.edu.unsam.algo3.service
 
 import ar.edu.unsam.algo3.domain.FiltroFigurita
 import ar.edu.unsam.algo3.dto.FiguritaDTO
+import ar.edu.unsam.algo3.dto.TipoFiguList
 import ar.edu.unsam.algo3.dto.toDTO
 import ar.edu.unsam.algo3.repository.FiguritasRepository
 import ar.edu.unsam.algo3.repository.UsuariosRepository
@@ -12,9 +13,8 @@ class FiguritaService(
   val figuritaRepository: FiguritasRepository,
   val usuariosRepository: UsuariosRepository
 ) {
-  fun obtenerFiguritasParaIntercambiar(filtro: FiltroFigurita): List<FiguritaDTO> {
-    val otros = this.otrosUsuarios(filtro.idUsuario)
-    var listaFiltrada = otros.flatMap { it.listaFiguritasARegalar().map { figu -> figu.toDTO(it) } }
+  fun filtrarFigus(figus: List<FiguritaDTO>, filtro: FiltroFigurita): List<FiguritaDTO>{
+    var listaFiltrada = figus
 
     if (filtro.palabraClave != "") {
       listaFiltrada = filtroPalabraClave(filtro.palabraClave, listaFiltrada)
@@ -33,6 +33,16 @@ class FiguritaService(
     }
 
     return listaFiltrada
+  }
+  fun obtenerFiguritasParaIntercambiar(filtro: FiltroFigurita): List<FiguritaDTO> {
+    val otros = this.otrosUsuarios(filtro.idUsuario)
+    var listaFigus = otros.flatMap { it.listaFiguritasARegalar().map { figu -> figu.toDTO(it) } }
+
+    return filtrarFigus(listaFigus,filtro)
+  }
+
+  fun obtenerFiguritasAgregables(logedUserID: Int, tipoFigus: TipoFiguList) {
+    val user = usuariosRepository.getById(logedUserID)
   }
 
   fun pedirValoracion (id: Int) = figuritaRepository.getById(id).valoracion() 
