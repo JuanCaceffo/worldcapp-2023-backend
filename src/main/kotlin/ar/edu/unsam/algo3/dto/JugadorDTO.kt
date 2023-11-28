25fd9907dad6017c1b9a9d25999556bd79b67494
lopez-sol <lopez.msol.07@gmail.com>
2023-11-28 03:28:05 -0300

    Endpoint para getAll jugadores hecho (controller, service, Dto, etc)

--- src/main/kotlin/ar/edu/unsam/algo3/dto/JugadorDTO.kt
+++ src/main/kotlin/ar/edu/unsam/algo3/dto/JugadorDTO.kt
@@ -2,6 +2,36 @@ package ar.edu.unsam.algo3.dto
 
 import ar.edu.unsam.algo3.domain.*
 
+data class JugadorDTO(
+    val id: Int,
+    val nombre: String,
+    val apellido: String,
+    val fechaNacimiento: String,
+    val altura: Double,
+    val peso: Double,
+    val nroCamiseta: Int,
+    val seleccion: String,
+    val anioDebut: String,
+    val posicion: String,
+    val esLider: Boolean,
+    val cotizacion: Double,
+)
+
+fun Jugador.toDTO() = JugadorDTO(
+    id = this.id,
+    nombre = this.nombre,
+    apellido = this.apellido,
+    fechaNacimiento = this.fechaNacimiento.toString(),
+    altura = this.altura,
+    peso = this.peso,
+    nroCamiseta = this.nroCamiseta,
+    seleccion = this.seleccionPerteneciente.pais,
+    anioDebut = this.anioDeDebut.toString(),
+    posicion = this.posicion.nombre,
+    esLider = this.esLider,
+    cotizacion = this.cotizacion
+)
+
 data class JugadorCreateDTO(
     val nombre: String,
     val apellido: String,
