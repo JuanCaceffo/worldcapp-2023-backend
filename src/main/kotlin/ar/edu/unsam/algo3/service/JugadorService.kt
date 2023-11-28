3b57cdad781ca02be840709ddc0b82caff994fd2
madescoces <madescoces@gmail.com>
2023-11-28 18:06:32 -0300

    Merge y corrección de test

--- src/main/kotlin/ar/edu/unsam/algo3/service/JugadorService.kt
+++ src/main/kotlin/ar/edu/unsam/algo3/service/JugadorService.kt
@@ -1,14 +1,9 @@
 package ar.edu.unsam.algo3.service
 
 import ar.edu.unsam.algo3.controller.BaseFilterParams
-import ar.edu.unsam.algo3.controller.FiguritaFilterParams
 import ar.edu.unsam.algo3.domain.*
 import ar.edu.unsam.algo3.dto.*
-import ar.edu.unsam.algo3.error.NotFoundException
-import ar.edu.unsam.algo3.repository.FiguritasRepository
 import ar.edu.unsam.algo3.repository.JugadorRepository
-import ar.edu.unsam.algo3.repository.MENSAJE_ERROR_ID_INEXISTENTE
-import ar.edu.unsam.algo3.repository.UsuariosRepository
 import org.springframework.stereotype.Service
 
 @Service
